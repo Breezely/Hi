f1323f82d25287d274f0dec9a7b3c61f0aa8f92b
Breezely <44201082+Breezely@users.noreply.github.com>
2019-01-11 21:29:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -239,10 +239,5 @@ try {
 });
 
 
-client.on ("guildMemberAdd", member => {
-  
-   var role = member.guild.roles.find ("name", "Relax");
-   member.addRole (role);
-
 
             client.login(process.env.BOT_TOKEN);
